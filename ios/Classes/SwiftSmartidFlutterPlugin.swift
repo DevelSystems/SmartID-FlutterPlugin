27382a3ed2bb1ec1ad17b31dc27cd5e1830d5bb0
José Ramírez <jramirez@develsystems.com>
2024-08-07 20:27:20 -0600

    - change swift bridge function

--- ios/Classes/SwiftSmartidFlutterPlugin.swift
+++ ios/Classes/SwiftSmartidFlutterPlugin.swift
@@ -60,35 +60,148 @@ public class SwiftSmartidFlutterPlugin: NSObject, FlutterPlugin {
             result(FlutterError(code: "INVALID_ARGUMENTS", message: "Invalid arguments received", details: nil))
             return
         }
-        do {
-            let operationData = try JSONSerialization.data(withJSONObject: operationDict, options: [])
+        
+        let cleanedOperationDict = operationDict.removingNullsAndProvidingDefaults()
 
-            let decoder = JSONDecoder()
-            do {
-                var operation = try decoder.decode(CoreOperation.self, from: operationData)
 
-                let newAccount = Account(
-                    client: operation.account?.client ?? "",
-                    clientRefId: 0,
-                    clientRefIdStr: "",
-                    email: "",
-                    phoneNumber: "",
-                    session: "",
-                    accountNumber: "",
-                    bank: ""
-                )
+        if var coreOperation = convertToCoreOperation(dict: cleanedOperationDict) {
+            let newAccount = Account(
+                client: coreOperation.account?.client ?? "",
+                clientRefId: 0,
+                clientRefIdStr: "",
+                email: "",
+                phoneNumber: "",
+                session: "",
+                accountNumber: "",
+                bank: ""
+            )
 
-                operation.account?.client = newAccount.client
-
-                SID.shared.createOperation(channel: license, operation: operation)
-            } catch let error {
-                result(FlutterError(code: "DECODE_ERROR", message: "Failed to decode operation", details: error.localizedDescription))
-                return
-            }
+            coreOperation.account?.client = newAccount.client
+            
+            SID.shared.createOperation(channel: license, operation: coreOperation)
             result("Create operation success")
-        } catch {
-            result(FlutterError(code: "DECODE_ERROR", message: "Failed to decode operation", details: error.localizedDescription))
+        } else {
+            result(FlutterError(code: "DECODE_ERROR", message: "Failed to convert operation dictionary", details: nil))
         }
     }
 
+    
+    private func convertToCoreOperation(dict: [String: Any]) -> CoreOperation? {
+        guard let channelId = dict["channelId"] as? String else { return nil }
+        let device = dict["device"] as? [String: Any]
+        let transaction = dict["transaction"] as? [String: Any]
+        let account = dict["account"] as? [String: Any]
+        let accountTo = dict["accountTo"] as? [String: Any]
+        let debitFrom = dict["debitFrom"] as? [String: Any]
+        let creditTo = dict["creditTo"] as? [String: Any]
+        let creditCard = dict["creditCard"] as? [String: Any]
+        let order = dict["order"] as? [String: Any]
+        let timestamp = dict["timestamp"] as? String
+
+        let coreOperation = CoreOperation(
+            channelId: channelId,
+            device: device != nil ? convertToDevice(dict: device!) : nil,
+            transaction: transaction != nil ? convertToTransaction(dict: transaction!) : nil,
+            account: account != nil ? convertToAccount(dict: account!) : nil,
+            accountTo: accountTo != nil ? convertToAccount(dict: accountTo!) : nil,
+            debitFrom: debitFrom != nil ? convertToBankAccount(dict: debitFrom!) : nil,
+            creditTo: creditTo != nil ? convertToBankAccount(dict: creditTo!) : nil,
+            creditCard: creditCard != nil ? convertToCreditCard(dict: creditCard!) : nil,
+            order: order != nil ? convertToOrder(dict: order!) : nil,
+            timestamp: timestamp
+        )
+
+        return coreOperation
+    }
+
+    private func convertToAccount(dict: [String: Any]) -> Account {
+        return Account(
+            client: dict["client"] as? String ?? "",
+            clientRefId: dict["clientRefId"] as? Int ?? 0,
+            clientRefIdStr: dict["clientRefIdStr"] as? String ?? "",
+            email: dict["email"] as? String ?? "",
+            phoneNumber: dict["phoneNumber"] as? String ?? "",
+            session: dict["session"] as? String ?? "",
+            accountNumber: dict["accountNumber"] as? String ?? "",
+            bank: dict["bank"] as? String ?? ""
+        )
+    }
+
+    private func convertToBankAccount(dict: [String: Any]) -> BankAccount {
+        return BankAccount(
+            account: dict["account"] as? String ?? "",
+            bank: dict["bank"] as? String ?? "",
+            currency: dict["currency"] as? String ?? ""
+        )
+    }
+
+    private func convertToTransaction(dict: [String: Any]) -> Transaction {
+        return Transaction(
+            category: dict["category"] as? String ?? "",
+            type: dict["type"] as? String ?? "",
+            description: dict["description"] as? String ?? "",
+            reference: dict["reference"] as? String ?? "",
+            date: dict["date"] as? String,
+            details: dict["details"] as? [String: String] ?? [:]
+        )
+    }
+
+    private func convertToDevice(dict: [String: Any]) -> Device {
+        return Device(
+            smartId: dict["smartId"] as? String ?? "",
+            ipAddress: dict["ipAddress"] as? String ?? "",
+            userAgent: dict["userAgent"] as? String ?? ""
+        )
+    }
+
+    private func convertToCreditCard(dict: [String: Any]) -> CreditCard {
+        return CreditCard(
+            bin: dict["bin"] as? String ?? "",
+            hash: dict["hash"] as? String ?? "",
+            last4Digits: dict["last4Digits"] as? String ?? "",
+            token: dict["token"] as? String ?? ""
+        )
+    }
+
+    private func convertToOrder(dict: [String: Any]) -> Order {
+        return Order(
+            amount: dict["amount"] as? Decimal ?? 0,
+            currency: dict["currency"] as? String ?? ""
+        )
+    }
+
+}
+
+extension Dictionary where Key == String {
+    func removingNullsAndProvidingDefaults() -> Dictionary {
+        var dict = self
+        let keysWithNullValues = dict.keys.filter { dict[$0] is NSNull || dict[$0] == nil }
+        for key in keysWithNullValues {
+            dict[key] = defaultForType(key: key)
+        }
+
+        for (key, value) in dict {
+            if let nestedDict = value as? [String: Any] {
+                dict[key] = nestedDict.removingNullsAndProvidingDefaults() as? Value
+            }
+        }
+        return dict
+    }
+    
+    private func defaultForType(key: String) -> Value? {
+        switch key {
+        case "client", "clientRefIdStr", "email", "phoneNumber", "session", "accountNumber", "bank", "category", "type", "description", "reference", "currency", "timeStamp", "bin", "hash", "last4Digits", "token", "smartId":
+            return "" as? Value
+        case "clientRefId", "channelId":
+            return 0 as? Value
+        case "amount":
+            return 0.0 as? Value
+        case "details":
+            return [:] as? Value
+        case "device", "transaction", "account", "accountTo", "debitFrom", "creditTo", "creditCard", "order":
+            return nil // Devolver nil para objetos anidados que serán manejados de otra manera
+        default:
+            return nil
+        }
+    }
 }
